7288e63f0efb7e24d1dfdbaf6380634395360eb9
Ersan Karimi <ersanb19@gmail.com>
2022-01-21 21:50:27 +0800

    Make Header component

---
+++ src/components/Header/Header.classes.js
@@ -0,0 +1,1 @@
+export const headerClasses = 'text-gray-800 text-4xl font-semibold'
---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { headerClasses } from './Header.classes'
+
+const Header = () => {
+	return <h1 className={headerClasses}>TodoList</h1>
+}
+
+export default Header
