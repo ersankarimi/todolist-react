e29cfc452a62379e2b303ed14334e47fa8b83990
Ersan Karimi <ersanb19@gmail.com>
2022-01-22 17:01:48 +0800

    Update ListItem.js

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -2,27 +2,45 @@ import React from 'react'
 import RemoveButton from './../Button/RemoveButton'
 import DoneButton from './../Button/DoneButton'
 import NotDoneButton from './../Button/NotDoneButton'
-import { state } from '../../reducer/state'
+import { ACTIONS } from './../../reducer/actions'
 
-const List = () => {
+const List = (props) => {
+	const { todo } = props
+	const handleDone = () => {
+		props.dispatch({
+			type: ACTIONS.TODO_DONE,
+			payload: {
+				id: todo.id,
+			},
+		})
+	}
+	const handleNotDone = () => {
+		props.dispatch({
+			type: ACTIONS.TODO_NOT_DONE,
+			payload: {
+				id: todo.id,
+			},
+		})
+	}
+
+	const handleRemoveTodo = () => {
+		props.dispatch({
+			type: ACTIONS.REMOVE_TODO,
+			payload: {
+				id: todo.id,
+			},
+		})
+	}
 	return (
-		<div id='todo-lists'>
-			{state.map((todo) => {
-				const { todoValue, complete, id } = todo
-				return (
-					<li
-						className='flex my-4 items-center bg-gray-100 p-2'
-						key={id}
-					>
-						<p className='w-full text-gray-700 text-xl'>
-							{todoValue}
-						</p>
-						{complete ? <DoneButton /> : <NotDoneButton />}
-						<RemoveButton />
-					</li>
-				)
-			})}
-		</div>
+		<li className='flex my-4 items-center bg-gray-100 p-2' key={todo.id}>
+			<p className='w-full text-gray-700 text-xl'>{todo.todoValue}</p>
+			{todo.complete ? (
+				<NotDoneButton handleNotDone={handleNotDone} />
+			) : (
+				<DoneButton handleDone={handleDone} />
+			)}
+			<RemoveButton handleRemoveTodo={handleRemoveTodo} />
+		</li>
 	)
 }
 
