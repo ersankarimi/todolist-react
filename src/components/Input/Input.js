6d1c65664249d4c83b8697890e8741fbb0396573
Ersan Karimi <ersanb19@gmail.com>
2022-01-22 17:01:30 +0800

    Update input.js

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,11 +1,9 @@
-import React, { useState, useReducer } from 'react'
-import { reducer } from './../../reducer/reducer'
-import { ACTIONS } from './../../reducer/actions'
-import { state } from './../../reducer/state'
+import React from 'react'
 
-const Input = () => {
-	const [todo, setTodo] = useState('')
-	const [todos, dispatch] = useReducer(reducer, state)
+const Input = ({ handleChange, todo }) => {
+	const handleChangeValue = (e) => {
+		handleChange(e.target.value)
+	}
 
 	return (
 		<input
@@ -13,9 +11,7 @@ const Input = () => {
 			className='shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-gray-500 focus:outline-none focus:border-gray-800'
 			placeholder='Add Todo'
 			value={todo}
-			onChange={(e) => {
-				setTodo(e.target.value)
-			}}
+			onChange={handleChangeValue}
 		/>
 	)
 }
