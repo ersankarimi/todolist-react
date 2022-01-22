5c120f2b5ca14b597b9a01866be8b46999ff97b2
Ersan Karimi <ersanb19@gmail.com>
2022-01-22 17:02:18 +0800

    Update reducer.js

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -2,13 +2,27 @@ import { ACTIONS } from './actions'
 export const reducer = (state, action) => {
 	switch (action.type) {
 		case ACTIONS.ADD_TODO:
-			return
+			return {
+				...state,
+				todos: [...state.todos, action.payload],
+			}
 		case ACTIONS.REMOVE_TODO:
-			return
+			return {
+				...state,
+				todos: [...state.todos].filter(
+					(todo) => todo.id !== action.payload.id
+				),
+			}
+
 		case ACTIONS.TODO_DONE:
-			return
 		case ACTIONS.TODO_NOT_DONE:
-			return
+			const anjay = state.todos.map((todo) => {
+				if (todo.id === action.payload.id) {
+					return { ...todo, complete: !todo.complete }
+				}
+				return todo
+			})
+			return { ...state, todos: anjay }
 
 		default:
 			return { ...state }
